8ec6ea9059cff33fbc9958dc67210a01ab1887f7
Ernesto Montellano <info@greda.digital>
2025-09-05 00:15:03 -0400

    Imagen de fondo en construcción

--- wp-content/themes/ele/landing.php
+++ wp-content/themes/ele/landing.php
@@ -218,4 +218,23 @@ get_header(); ?>
     </footer>
 </main>
 
+<!-- Google Calendar Appointment Scheduling begin -->
+<link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet">
+<script src="https://calendar.google.com/calendar/scheduling-button-script.js" async></script>
+<script>
+    (function() {
+        var target = document.currentScript;
+        window.addEventListener('load', function() {
+            calendar.schedulingButton.load({
+                url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ18rAPSDOo4f2nxVDn2Ps74qwyOCRGFNsZPFVj1fgEIfjPR8ORiPf0XBtogrOhlu6zisIFrUcru?gv=true',
+                color: '#039BE5',
+                label: "Agenda aqu\xED",
+                target,
+            });
+        });
+    })();
+</script>
+<!-- end Google Calendar Appointment Scheduling -->
+
+
 <?php get_footer(); ?>
--- wp-content/themes/ele/underconstruction.php
+++ wp-content/themes/ele/underconstruction.php
@@ -3,8 +3,11 @@
 
 get_header();
 ?>
-<main class="bg-primario h-screen-safe">
-    <h1>En construcción</h1>
+<main class="bg-primario h-screen-safe flex flex-col justify-center items-center">
+    <div class="w-[300px] h-[300px] bg-[url(../images/logo_blanco.svg)] bg-no-repeat bg-center bg-contain">
+
+    </div>
+    <h2 class="text-white letter tracking-widest">EN CONSTRUCCIÓN</h2>
 
 </main>
 
